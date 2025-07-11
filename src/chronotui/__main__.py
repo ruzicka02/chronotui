b5ca49050b1f76b079800b5bec069a2190ab8f78
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2025-07-11 20:15:41 +0200

    feat: Title, subtitle

--- src/chronotui/__main__.py
+++ src/chronotui/__main__.py
@@ -8,4 +8,6 @@ if __name__ == "__main__":
     import logging
     logging.basicConfig(level=logging.INFO, filename="chronotui.log",)
     app = StopwatchApp()
+    app.title="ChronoTUI"
+    app.sub_title="Track your time with style"
     app.run()
